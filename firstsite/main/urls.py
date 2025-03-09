3243594c6475d87125025fece84d898259901814
hom9ki <156588482+hom9ki97@users.noreply.github.com>
2025-03-09 14:11:21 +0300

    Добавление контроллеров и страниц для ткм и уроков

--- firstsite/main/urls.py
+++ firstsite/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, other_page, FSLoginView, profile, FSLogout
+from .views import index, other_page, FSLoginView, profile, FSLogout, topic_page, lesson_page
 
 
 app_name = 'main'
@@ -9,6 +9,8 @@ urlpatterns = [
     path('accounts/logout/', FSLogout.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', FSLoginView.as_view(), name='login'),
+    path('topic/lesson/<int:lesson_id>/', lesson_page, name='lesson'),
+    path('<str:page>/', topic_page, name='topic'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index')
 ]
--- firstsite/main/views.py
+++ firstsite/main/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
+
+from .models import Topic, Lessons
 
 
 @login_required
@@ -29,3 +31,19 @@ def other_page(request, page):
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def topic_page(request, page):
+    topics = Topic.objects.prefetch_related('lessons').all()
+    context = {'topics': topics}
+    return render(request, 'main/topic.html', context)
+
+
+def lesson_page(request, lesson_id):
+    lesson = get_object_or_404(Lessons, id=lesson_id)
+    homeworks = lesson.homeworks.all()
+    context = {
+        'lesson': lesson,
+        'homeworks': homeworks,
+    }
+    return render(request, 'main/lesson.html', context)
